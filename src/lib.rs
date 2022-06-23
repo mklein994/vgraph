a0ecf6271c5aac7823a779a6183b5f1fa8236c0c
Matthew Klein <mklein994@gmail.com>
2022-06-22 21:30:09 -0500

    Refactor input handling to separate module

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 mod error;
+mod graph;
 
 use self::error::Error;
-use atty::Stream;
+
 use getopts::Options;
 use std::env;
-use std::io::{self, Read};
 
 pub type Result<T> = std::result::Result<T, Error>;
 
@@ -40,48 +40,7 @@ pub fn run() -> Result<()> {
         return Ok(());
     }
 
-    // Gather stdin if available, otherwise just an empty string. If run in a pipe, stdin
-    // isn't available for the user to enter something with the keyboard for example.
-    // Wait for input if no numbers were given though.
-    let buf = if !matches.opt_present("no-wait")
-        && (atty::isnt(Stream::Stdin) || matches.free.is_empty())
-    {
-        let mut stdin = io::stdin();
-        let mut buf = String::new();
-
-        stdin.read_to_string(&mut buf)?;
-        buf
-    } else {
-        String::new()
-    };
-
-    // Using the free parameters provided on the command line first, convert that and each
-    // line from stdin into a list of numbers.
-    let numbers: Vec<f64> = matches
-        .free
-        .iter()
-        .map(String::as_str)
-        .chain(buf.lines())
-        .filter_map(|x| x.parse().ok())
-        .collect();
-
-    // Find the highest and lowest values in the list
-    let (min, max) = if numbers.len() == 1 {
-        if numbers[0] > 1_f64 || numbers[0] < 0_f64 {
-            return Err(Error::OutOfBounds);
-        }
-        (0_f64, 1_f64)
-    } else {
-        numbers
-            .iter()
-            .fold((std::f64::MAX, std::f64::MIN), |(min, max), &x| {
-                (x.min(min), x.max(max))
-            })
-    };
-
-    for line in numbers.into_iter().map(|n| scale(min, max, n)) {
-        print!("{}", graph(line)?);
-    }
+    graph::scaled(&matches)?;
 
     if !matches.opt_present("no-newline") {
         println!();
